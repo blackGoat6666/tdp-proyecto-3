8ce3b21d4f5a3ccf7fcb2ba55dcf5fa651347e9f
Iara <iaravera3333@gmail.com>
2021-11-04 06:55:57 -0300

    grafica los dots

--- Fifteen-chan_game/src/gui/GUI.java
+++ Fifteen-chan_game/src/gui/GUI.java
@@ -3,6 +3,7 @@ package gui;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.EventQueue;
+import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -10,6 +11,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+
 import javax.swing.JLabel;
 
 public class GUI {
@@ -17,8 +20,31 @@ public class GUI {
 	private JPanel panelMenu;
 	private JPanel panelJuego;
 	private JLabel labels[][] = new JLabel[20][20];
+	private JLabel labels2[][] = new JLabel[20][20];
+	private JLabel labels3[][] = new JLabel[20][20];
 
 	private JFrame frame;
+	private JPanel grillaNivel3;
+	
+	private Point [] paredes = {new Point(1,1), new Point(2,1), new Point(3,1), new Point(4,1), new Point(1,2), new Point(2,2), new Point(3,2), new Point(4,2),new Point(5,4),new Point(14,4),
+			new Point(5,5),new Point(14,5),new Point(5,7),new Point(14,7),
+			new Point(15,1), new Point(16,1), new Point(17,1), new Point(18,1), new Point(15,2), new Point(16,2), new Point(17,2), new Point(18,2),
+			new Point(0,4), new Point(1,4), new Point(2,4), new Point(3,4),  new Point(16,4), new Point(17,4), new Point(18,4), new Point(19,4),
+			new Point(10,0),new Point(10,1), new Point(10,2),new Point(9,0),new Point(9,1), new Point(9,2),new Point(7,1),new Point(7,2), new Point(6,2),
+			new Point(12,2),new Point(12,1),new Point(13,2),new Point(7,4),  new Point(8,4), new Point(9,4), new Point(10,4), new Point(11,4),new Point(12,4),
+			new Point(9,5),new Point(9,6),new Point(12,2),new Point(12,1),new Point(1,18),new Point(2,18),new Point(3,18),new Point(4,18),new Point(5,18),new Point(6,18),
+			new Point(7,18),new Point(9,18),new Point(11,18),new Point(12,18),new Point(13,18),new Point(14,18),new Point(15,18),new Point(16,18),new Point(17,18),new Point(18,18),
+			new Point(1,6),new Point(2,6),new Point(3,6),new Point(4,6),new Point(5,6),new Point(6,6),new Point(7,6),new Point(18,6),new Point(17,6),new Point(16,6),new Point(15,6),new Point(14,6),
+			new Point(12,6),new Point(13,6),
+			new Point(0,8),new Point(1,8),new Point(2,8),new Point(3,8),new Point(5,8),new Point(7,8),new Point(8,8),new Point(9,8),new Point(10,8),new Point(11,8),new Point(12,8),
+			new Point(14,8),new Point(16,8),new Point(17,8),new Point(18,8),new Point(19,8),new Point(0,9),new Point(1,9),new Point(2,9),new Point(3,9),new Point(7,9),new Point(8,9),new Point(9,9),new Point(10,9),new Point(11,9),new Point(12,9),
+			new Point(16,9),new Point(17,9),new Point(18,9),new Point(19,9), new Point(0,11),new Point(1,11),new Point(2,11),new Point(3,11),new Point(5,10),new Point(7,10),new Point(8,10),new Point(9,10),new Point(10,10),new Point(11,10),new Point(12,10),
+			new Point(14,10),new Point(14,11),new Point(5,11),new Point(16,11),new Point(17,11),new Point(18,11),new Point(19,11),
+			new Point(0,12),new Point(1,12),new Point(2,12),new Point(3,12),new Point(5,12),new Point(7,12),new Point(8,12),new Point(9,12),new Point(10,12),new Point(11,12),new Point(12,12),
+			new Point(14,12),new Point(16,12),new Point(17,12),new Point(18,12),new Point(19,12),new Point(10,13),new Point(10,14),
+			new Point(7,14),new Point(6,14),new Point(5,14),new Point(3,14),new Point(2,14),new Point(1,14),new Point(12,14),new Point(13,14),new Point(14,14),new Point(16,14),new Point(17,14),new Point(18,14),new Point(3,15),new Point(16,15),
+			new Point(0,16),new Point(1,16),new Point(3,16),new Point(5,16),new Point(7,16),new Point(8,16),new Point(9,16),new Point(10,16),new Point(11,16),new Point(12,16),new Point(14,16),new Point(16,16),new Point(18,16),new Point(19,16),
+			new Point(5,17),new Point(9,17),new Point(14,17)};
 
 	/**
 	 * Launch the application.
@@ -76,20 +102,16 @@ private void initialize() {
 		panelJuego.add(grillaNivel1);
 		
 		JPanel grillaNivel2 = new Panel("/Images/nivelvampiro2.png");
+		grillaNivel2.setSize(660, 660);
+		grillaNivel2.setLocation(365, 10);
 		grillaNivel1.setBounds(365, 10, 660, 660);
 		panelJuego.add(grillaNivel2);
 		
-		JPanel grillaNivel3 = new Panel("/Images/nivelvampiro3.png");
+		grillaNivel3 = new Panel("/Images/nivelvampiro3.png");
+		grillaNivel3.setSize(660, 660);
+		grillaNivel3.setLocation(365, 10);
 		grillaNivel1.setBounds(365, 10, 660, 660);
 		panelJuego.add(grillaNivel3);
-		
-		
-		
-		JButton btnJugarMetalero = new JButton("Jugar metal fight");
-		btnJugarMetalero.setForeground(Color.RED);
-		btnJugarMetalero.setBackground(Color.BLACK);
-		btnJugarMetalero.setBounds(314, 238, 226, 58);
-		panelMenu.add(btnJugarMetalero);
 		
 		JButton btnAyuda = new JButton("Ayuda");
 		btnAyuda.setForeground(Color.RED);
@@ -103,6 +125,13 @@ private void initialize() {
 		btnSalir.setSelectedIcon(null);
 		btnSalir.setBounds(406, 451, 226, 58);
 		panelMenu.add(btnSalir);
+		
+		JButton btnJugarMetalero = new JButton("Jugar metal fight");
+		btnJugarMetalero.setForeground(Color.RED);
+		btnJugarMetalero.setBackground(Color.BLACK);
+		btnJugarMetalero.setBounds(314, 238, 226, 58);
+		panelMenu.add(btnJugarMetalero);
+		
 		
 		JButton btnJugarVampiros = new JButton("Jugar Jardin de sangre");
 		btnJugarVampiros.setForeground(Color.RED);
@@ -154,7 +183,37 @@ private void initialize() {
 			labels[filas][columna].setIcon(new ImageIcon(GUI.class.getResource("/Images/dotVampiro.png")));
 		}
 		}
+		for (int i = 0; i<paredes.length; i++) {
+			labels[(int) paredes[i].getY()][(int) paredes[i].getX()].setVisible(false);
+		}
 		
+		for (int filas =0; filas < 20; filas++){
+			for (int columna =0; columna < 20; columna++) {
+				labels2[filas][columna] = new JLabel("");
+			    labels2[filas][columna].setBounds(40+30*columna, 30+30*filas , 30, 30);
+				grillaNivel2.add(labels2[filas][columna]);
+				labels2[filas][columna].setVisible(true);
+				labels2[filas][columna].setIcon(new ImageIcon(GUI.class.getResource("/Images/dotVampiro.png")));
+			}
+			}
+			for (int i = 0; i<paredes.length; i++) {
+				labels2[(int) paredes[i].getY()][(int) paredes[i].getX()].setVisible(false);
+		}
+
+			for (int filas =0; filas < 20; filas++){
+				for (int columna =0; columna < 20; columna++) {
+					labels3[filas][columna] = new JLabel("");
+				    labels3[filas][columna].setBounds(40+30*columna, 30+30*filas , 30, 30);
+					grillaNivel3.add(labels3[filas][columna]);
+					labels3[filas][columna].setVisible(true);
+					labels3[filas][columna].setIcon(new ImageIcon(GUI.class.getResource("/Images/dotVampiro.png")));
+				}
+				}
+				for (int i = 0; i<paredes.length; i++) {
+					labels3[(int) paredes[i].getY()][(int) paredes[i].getX()].setVisible(false);
+				}
+		
+			
 		
 		// acciones botones
 		
