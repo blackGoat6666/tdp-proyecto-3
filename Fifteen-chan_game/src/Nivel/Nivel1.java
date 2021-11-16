cc02d5c97d2363664e8a0e2577a1d5465dabbf29
Iara <iaravera3333@gmail.com>
2021-11-16 02:42:53 -0300

    termine nivel 1, falta implementar el resto de clases entidad para que no tire error

--- Fifteen-chan_game/src/Nivel/Nivel1.java
+++ Fifteen-chan_game/src/Nivel/Nivel1.java
@@ -29,8 +29,27 @@ public class Nivel1 extends NivelAbstracto{
 	}
 	
 	public Bloque [][] getMatriz () {
+		Bloque [][] matriz = new Bloque [20][20];
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+				switch (matrizNivel[i][j]) {
+				case 0:
+					matriz[i][j] = new Bloque(new Pared());
+					break;
+				case 1:
+					matriz[i][j] = new Bloque(new Dot());
+					break;
+				case 2:
+					matriz[i][j] = new Bloque(new PowerPellet());
+					break;
+				case 3:
+					matriz[i][j] = new Bloque(new Gate());
+					break;
+				}
+			}
+	    }
 		
-		return null;
+		return matriz;
 		
 	}
 
